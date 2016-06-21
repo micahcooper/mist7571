5994ff3fdb670f2eff3197354a65798471531ffe
Micah Cooper <mc@micahcooper.com>
2016-06-21 15:58:45 -0400

    initial commit

---
+++ Assignment01/calculateProfitability.js
@@ -0,0 +1,9 @@
+function caculateProfitability()
+{
+	profit = document.getElementById("profit");
+	averageNumberOfShares = document.getElementById("averageNumberOfShares");
+	
+	pricePerShare = document.getElementById("pricePerShare");
+	totalAssets = document.getElementById("totalAssets")
+	window.alert( x.value );
+}
