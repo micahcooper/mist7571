4ed7fe41ffcbba440f688761b4db1e9698c173ad
Micah Cooper <mc@micahcooper.com>
2016-06-21 22:21:14 -0400

    updated

--- Assignment01/calculateProfitability.js
+++ Assignment01/calculateProfitability.js
@@ -1,9 +1,26 @@
-function caculateProfitability()
+function calculateProfitability()
 {
-	profit = document.getElementById("profit");
-	averageNumberOfShares = document.getElementById("averageNumberOfShares");
+	console.log("starting the program");
+	profit = document.getElementById("profit").value;
+	console.log("profit - "+profit);
+	averageNumberOfShares = document.getElementById("averageNumberOfShares").value;
 	
-	pricePerShare = document.getElementById("pricePerShare");
-	totalAssets = document.getElementById("totalAssets")
-	window.alert( x.value );
+	pricePerShare = document.getElementById("pricePerShare").value;
+	totalAssets = document.getElementById("totalAssets").value;
+	//intangibleAssets = document.getElementById("intangibleAssets").value;
+	//liabilities = document.getElementById("liabilities").value;
+
+	earningsPerShare = document.getElementById("earningsPerShare").value;
+	//peRatio = document.getElementById("peRatio");
+	percentageExpectedGrowth = document.getElementById("percentageExpectedGrowth").value;
+	
+	earningsPerShare = calculateEarningsPerShare(profit, averageNumberOfShares);
+	console.log(earningsPerShare);
+	//priceToBook = calculatePriceToBook();
+	
+}
+
+function calculateEarningsPerShare(profit, averageNumberOfShares)
+{
+	return profit / averageNumberOfShares;
 }
