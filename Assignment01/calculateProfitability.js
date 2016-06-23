22e194b58c86e16882418ab94fee87cfb9f9a3f9
Micah Cooper <mc@micahcooper.com>
2016-06-22 22:21:09 -0400

    updated files

--- Assignment01/calculateProfitability.js
+++ Assignment01/calculateProfitability.js
@@ -14,19 +14,32 @@ function calculateProfitability()
 	//peRatio = document.getElementById("peRatio");
 	percentageExpectedGrowth = document.getElementById("percentageExpectedGrowth").value;
 	
-	earningsPerShare = calculateEarningsPerShare(profit, averageNumberOfShares);
+	earningsPerShare = profit / averageNumberOfShares;
 	console.log(earningsPerShare);
-	priceToBook = calculatePriceToBook(pricePerShare,totalAssets,intangibleAssets,liabilities);
-	console.log("price to book - "+priceToBook);
+	priceToBook = pricePerShare / (totalAssets - (intangibleAssets + liabilities));
+	console.log("price to book - "+priceToBook+" pricePerShare,totalAssets,intangibleAssets,liabilities"+pricePerShare+" "+totalAssets+" "+intangibleAssets+" "+liabilities);
+	
+	peRatio = pricePerShare / earningsPerShare;
+	
+	priceToEarningsGrowth = peRatio / percentageExpectedGrowth;
+	
+	console.log("PE Ratio: "+peRatio);
+	console.log("Price to earnings growth: "+priceToEarningsGrowth);
+	
+	outputResults(earningsPerShare, priceToBook);
 	
 }
 
-function calculateEarningsPerShare(profit, averageNumberOfShares)
+function outputResults()
 {
-	return profit / averageNumberOfShares;
-}
-
-function calculatePriceToBook(pricePerShare,totalAssets,intangibleAssets,liabilities)
-{
-	return pricePerShare / (totalAssets / intangibleAssets+liabilities);
+	document.write("<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\" /><link rel=\"stylesheet\" href=\"style.css\" /><title>CompanyProfitability</title></head>");
+	document.write("<header><h1>Results</h1></header>");
+	document.write("<table><tr><th>Name of Field</th><th>Value</th></tr>");
+	document.write("<tr><td></td><td>"+earningsPerShare+"</td></tr>");
+	document.write("<tr><td>Earnings Per Share</td><td>"+earningsPerShare+"</td></tr>");
+	document.write("<tr><td>Price to Book</td><td>"+priceToBook+"</td></tr>");
+	document.write("<tr><td>PE Ratio</td><td>"+peRatio+"</td></tr>");
+	document.write("<tr><td>Price to earnings growth</td><td>"+priceToEarningsGrowth+"</td></tr>");
+	document.write("</table>");
+	document.close();
 }
