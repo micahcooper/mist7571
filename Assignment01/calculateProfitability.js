ae9b9062362fa3f3309d5cad9cb54b92d90bef10
Micah Cooper <mc@micahcooper.com>
2016-06-24 21:49:41 -0400

    update

--- Assignment01/calculateProfitability.js
+++ Assignment01/calculateProfitability.js
@@ -1,18 +1,15 @@
 //function to calcuate the results based on a companies share and earnings report
 function calculateProfitability()
 {
-	console.log("starting the program");
+	console.log("starting the calculation");
 	profit = document.getElementById("profit").value;
 	console.log("profit - "+profit);
-	averageNumberOfShares = document.getElementById("averageNumberOfShares").value;
 	
+	averageNumberOfShares = document.getElementById("averageNumberOfShares").value;
 	pricePerShare = document.getElementById("pricePerShare").value;
 	totalAssets = document.getElementById("totalAssets").value;
 	intangibleAssets = document.getElementById("intangibleAssets").value;
 	liabilities = document.getElementById("liabilities").value;
-
-	//earningsPerShare = document.getElementById("earningsPerShare").value;
-	//peRatio = document.getElementById("peRatio");
 	percentageExpectedGrowth = document.getElementById("percentageExpectedGrowth").value;
 	
 	earningsPerShare = profit / averageNumberOfShares;
@@ -38,22 +35,4 @@ function calculateProfitability()
 	document.getElementById("returnOnEquity").innerHTML = returnOnEquity.toFixed(2);
 	//outputResults();
 	
-}
-
-//send the results to the document.write
-function outputResults()
-{
-	document.open();
-	document.write("<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\" /><link rel=\"stylesheet\" href=\"style.css\" /><title>CompanyProfitability</title></head>");
-	document.write("<header><h1>Results</h1></header>");
-	document.write("<table><tr><th>Name of Field</th><th>Value</th></tr>");
-	document.write("<tr><td>Earnings Per Share</td><td>"+earningsPerShare.toFixed(2)+"</td></tr>");
-	document.write("<tr><td>Price to Book</td><td>"+priceToBook.toFixed(2)+"</td></tr>");
-	document.write("<tr><td>PE Ratio</td><td>"+peRatio.toFixed(2)+"</td></tr>");
-	document.write("<tr><td>Price to earnings growth</td><td>"+priceToEarningsGrowth.toFixed(2)+"</td></tr>");
-	document.write("<tr><td>Return on equity</td><td>"+returnOnEquity.toFixed(2)+"</td></tr>");
-	document.write("</table>");
-	document.write("<p><a href=asg1.html>Go Back</a></p>");
-	document.write("</body></html>");
-	document.close();
 }
