4c56870d97c95e97505d48497180025fb3963fcf
Micah Cooper <mc@micahcooper.com>
2016-06-24 21:28:32 -0400

    updates

--- Assignment01/calculateProfitability.js
+++ Assignment01/calculateProfitability.js
@@ -17,28 +17,33 @@ function calculateProfitability()
 	
 	earningsPerShare = profit / averageNumberOfShares;
 	console.log(earningsPerShare);
+	document.getElementById("earningsPerShare").innerHTML = earningsPerShare.toFixed(2);
+	
 	priceToBook = pricePerShare / (totalAssets - (intangibleAssets + liabilities));
 	console.log("price to book - "+priceToBook+" pricePerShare,totalAssets,intangibleAssets,liabilities"+pricePerShare+" "+totalAssets+" "+intangibleAssets+" "+liabilities);
+	document.getElementById("priceToBook").innerHTML = priceToBook.toFixed(2);
 	
 	peRatio = pricePerShare / earningsPerShare;
+	console.log("PE Ratio - "+peRatio);
+	document.getElementById("peRatio").innerHTML = peRatio.toFixed(2);
 	
 	priceToEarningsGrowth = peRatio / percentageExpectedGrowth;
+	console.log("Price to Earnings Growth - "+priceToEarningsGrowth);
+	document.getElementById("priceToEarningsGrowth").innerHTML = priceToEarningsGrowth.toFixed(2);
 	
-	console.log("PE Ratio: "+peRatio);
-	console.log("Price to earnings growth: "+priceToEarningsGrowth);
-	
-	//netIncome = document.getElementById("netIncome").value;
 	bookValue = totalAssets - liabilities;
-	
 	returnOnEquity = profit / bookValue;
-	
-	outputResults();
+	console.log("Book Value - "+bookValue);
+	console.log("Return On Equity - "+returnOnEquity);
+	document.getElementById("returnOnEquity").innerHTML = returnOnEquity.toFixed(2);
+	//outputResults();
 	
 }
 
 //send the results to the document.write
 function outputResults()
 {
+	document.open();
 	document.write("<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\" /><link rel=\"stylesheet\" href=\"style.css\" /><title>CompanyProfitability</title></head>");
 	document.write("<header><h1>Results</h1></header>");
 	document.write("<table><tr><th>Name of Field</th><th>Value</th></tr>");
