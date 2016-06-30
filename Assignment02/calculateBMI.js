0c7de2b094b7249c70bdaf66b0e6ed27aa824e26
Micah Cooper <mc@micahcooper.com>
2016-06-29 22:18:38 -0400

    updated

--- Assignment02/calculateBMI.js
+++ Assignment02/calculateBMI.js
@@ -9,12 +9,50 @@ function calculateBMI()
 	console.log("weight: "+weight+" height: "+height)
 	bmi = (weight*703)/(height*height);
 	
-	if( bmi > 25 )
-		document.getElementById("overweight").style.backgroundColor += "yellow";
-		
+	var selectedRow;
+	
+	if( bmi < 18.5)
+	{
+		document.getElementById("underweight").style.backgroundColor = "yellow";
+		document.getElementById("normalweight").style.backgroundColor = "";
+		document.getElementById("overweight").style.backgroundColor = "";
+		document.getElementById("obesity").style.backgroundColor = "";
+	}
+	if( bmi >= 18.5 && bmi < 25)
+	{
+		document.getElementById("underweight").style.backgroundColor = "";
+		document.getElementById("normalweight").style.backgroundColor = "yellow";
+		document.getElementById("overweight").style.backgroundColor = "";
+		document.getElementById("obesity").style.backgroundColor = "";
+	}
+	if( bmi >= 25 && bmi < 30)
+	{
+		document.getElementById("underweight").style.backgroundColor = "";
+		document.getElementById("normalweight").style.backgroundColor = "";
+		document.getElementById("overweight").style.backgroundColor = "yellow";
+		document.getElementById("obesity").style.backgroundColor = "";
+	}
+	if( bmi >= 30)
+	{
+		document.getElementById("underweight").style.backgroundColor = "";
+		document.getElementById("normalweight").style.backgroundColor = "";
+		document.getElementById("overweight").style.backgroundColor = "";
+		document.getElementById("obesity").style.backgroundColor = "yellow";
+	}	
+	
+	document.getElementById("section-result").className = "";
 	document.getElementById("bmi-result").innerHTML = "<strong>"+bmi+"</strong";
-		
+	
+	
 	console.log( bmi );
+}
+
+function resetBMI()
+{
+	document.getElementById("section-result").className = "hide";
 	
-	
+	document.getElementById("underweight").style.backgroundColor = "";
+	document.getElementById("normalweight").style.backgroundColor = "";
+	document.getElementById("overweight").style.backgroundColor = "";
+	document.getElementById("obesity").style.backgroundColor = "";
 }
