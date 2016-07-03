a99b21da595224a10db7610d0564f88ed7f7ca55
Micah Cooper <mc@micahcooper.com>
2016-07-03 15:50:33 -0400

    updated

--- Assignment02/calculateBMI.js
+++ Assignment02/calculateBMI.js
@@ -11,49 +11,53 @@ function calculateBMI()
 	
 	var bmi = (weight*703)/(height*height);
 
-
-	
-	//this if-block resets status table rows and highlights the appropriate status row
-	if( bmi < 18.5)
-	{
-		document.getElementById("underweight").style.backgroundColor = "yellow";
-		document.getElementById("normalweight").style.backgroundColor = "";
-		document.getElementById("overweight").style.backgroundColor = "";
-		document.getElementById("obesity").style.backgroundColor = "";
-		message = "You are underweight you may want to talk to your GP about gaining weight. Being underweight can increase your risk of health problems.";
-
+	if( !bmi > 0 ){ 
+		document.getElementById("section-bmi").className = "";
+		document.getElementById("bmi-result").innerHTML = "<p>You entered an incorrect height or weight, please try again.</p>";
 	}
-	if( bmi >= 18.5 && bmi < 25)
-	{
-		document.getElementById("underweight").style.backgroundColor = "";
-		document.getElementById("normalweight").style.backgroundColor = "yellow";
-		document.getElementById("overweight").style.backgroundColor = "";
-		document.getElementById("obesity").style.backgroundColor = "";
-		message = "For most adults, an ideal BMI is in the 18.5-24.9 range. You are in this range.";
+	else{
+	
+		//this if-block resets status table rows and highlights the appropriate status row
+		if( bmi < 18.5)
+		{
+			document.getElementById("underweight").style.backgroundColor = "yellow";
+			document.getElementById("normalweight").style.backgroundColor = "";
+			document.getElementById("overweight").style.backgroundColor = "";
+			document.getElementById("obesity").style.backgroundColor = "";
+			message = "You are underweight you may want to talk to your GP about gaining weight. Being underweight can increase your risk of health problems.";
 
-	}
-	if( bmi >= 25 && bmi < 30)
-	{
-		document.getElementById("underweight").style.backgroundColor = "";
-		document.getElementById("normalweight").style.backgroundColor = "";
-		document.getElementById("overweight").style.backgroundColor = "yellow";
-		document.getElementById("obesity").style.backgroundColor = "";
-		message = "You are in the overweight category, you should think about losing weight. Being overweight can increase your risk of health problems.";
+		}
+		if( bmi >= 18.5 && bmi < 25)
+		{
+			document.getElementById("underweight").style.backgroundColor = "";
+			document.getElementById("normalweight").style.backgroundColor = "yellow";
+			document.getElementById("overweight").style.backgroundColor = "";
+			document.getElementById("obesity").style.backgroundColor = "";
+			message = "For most adults, an ideal BMI is in the 18.5-24.9 range. You are in this range.";
 
+		}
+		if( bmi >= 25 && bmi < 30)
+		{
+			document.getElementById("underweight").style.backgroundColor = "";
+			document.getElementById("normalweight").style.backgroundColor = "";
+			document.getElementById("overweight").style.backgroundColor = "yellow";
+			document.getElementById("obesity").style.backgroundColor = "";
+			message = "You are in the overweight category, you should think about losing weight. Being overweight can increase your risk of health problems.";
+
+		}
+		if( bmi >= 30)
+		{
+			document.getElementById("underweight").style.backgroundColor = "";
+			document.getElementById("normalweight").style.backgroundColor = "";
+			document.getElementById("overweight").style.backgroundColor = "";
+			document.getElementById("obesity").style.backgroundColor = "yellow";
+			message = "You are in the obese weight BMI category. Associated health problems include heart disease, stroke and type 2 diabetes.";
+		}	
+		
+		document.getElementById("section-bmi").className = "";
+		document.getElementById("bmi-result").innerHTML = "<p class=\"resultp\"><strong>Your BMI - </strong>"+bmi.toFixed(1)+"</p><p class=\"message\">"+message+"</p>";
+	
 	}
-	if( bmi >= 30)
-	{
-		document.getElementById("underweight").style.backgroundColor = "";
-		document.getElementById("normalweight").style.backgroundColor = "";
-		document.getElementById("overweight").style.backgroundColor = "";
-		document.getElementById("obesity").style.backgroundColor = "yellow";
-		message = "You are in the obese weight BMI category. Associated health problems include heart disease, stroke and type 2 diabetes.";
-	}	
-	
-	document.getElementById("section-bmi").className = "";
-	document.getElementById("bmi-result").innerHTML = "<p class=\"resultp\"><strong>Your BMI - </strong>"+bmi.toFixed(1)+"</p><p class=\"message\">"+message+"</p>";
-	
-	
 	console.log( "bmi number: "+bmi );
 	console.log( "calculation has ended" );
 }
